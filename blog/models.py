676df69c24ef581e98cf8416132c820c981cf78b
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-02-07 17:07:06 +0200

    Create a blog post model
    For post model also add:
      1. publish() function
      2. __str__() function

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 """Models for project."""
 
-from django.db import models  # noqa: F401
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """Model that represents post in our blog."""
+
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """Publish the post."""
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        """Return title of Post."""
+        return self.title
