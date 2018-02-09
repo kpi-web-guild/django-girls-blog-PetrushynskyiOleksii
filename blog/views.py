d8797886e2d2d7b38ba67392c58ba32d8ad62bd2
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-02-09 15:02:37 +0200

    Improve post_list function
     1. Correct docstring
     2. Get posts from DB sorted by published date
     3. Return posts as context

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 """Views for blog app."""
 
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
-    """Render the post list view."""
-    return render(request, 'blog/post_list.html', {})
+    """Render page with list of posts."""
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
