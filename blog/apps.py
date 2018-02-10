35bec7f3bbb96b74f77537f22ea4fc8c63e16655
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-02-10 16:04:32 +0200

    Integrate i18n in project

--- blog/apps.py
+++ blog/apps.py
@@ -1,8 +1,10 @@
 """Configuration of app."""
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class BlogConfig(AppConfig):
     """Configuration for Blog app."""
 
     name = 'blog'
+    vebose_name = _(u'Django Girls blog')
--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,22 @@
 
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext as _
 
 
 class Post(models.Model):
     """Model that represents post in our blog."""
 
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey('auth.User',
+                               verbose_name=_(u'Author'),
+                               on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_(u'Title'), max_length=200)
+    text = models.TextField(verbose_name=_(u'Text'),)
+    created_date = models.DateTimeField(verbose_name=_(u'Created date'),
+                                        default=timezone.now)
+    published_date = models.DateTimeField(verbose_name=_(u'Published date'),
+                                          blank=True,
+                                          null=True)
 
     def publish(self):
         """Publish the post."""
