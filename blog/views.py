541820ab529e9461ae4b613ba7bd9aaaa3b6b620
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2018-02-10 13:39:53 +0200

    Create post's form view

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -16,3 +17,9 @@ def post_detail(request, pk):
     """Render page with details of specific post."""
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """Render page with form of post."""
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
