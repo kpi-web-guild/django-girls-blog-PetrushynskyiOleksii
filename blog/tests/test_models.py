980f922d8ba48bd719c9a6da70846d3ea29fd92f
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-17 22:49:41 +0200

    Improve tests for publish post

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,7 +1,10 @@
 """Tests for models."""
+from datetime import datetime
+from unittest.mock import patch
 
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
 
 from blog.models import Post
 
@@ -21,9 +24,17 @@ class PostModelTest(TestCase):
         del self.user
         del self.test_post
 
+    @patch('django.utils.timezone.now', lambda: datetime(day=1,
+                                                         month=1,
+                                                         year=2017,
+                                                         tzinfo=timezone.get_current_timezone()))
     def test_post_publish(self):
         """Post published successfully."""
         self.test_post.publish()
+        self.assertEqual(self.test_post.published_date, datetime(day=1,
+                                                                 month=1,
+                                                                 year=2017,
+                                                                 tzinfo=timezone.get_current_timezone()))
 
     def test_post_str(self):
         """Post is rendered as its title."""
