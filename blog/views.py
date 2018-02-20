3d23829244282fba8f51f30c133105e002221077
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-20 10:41:17 +0200

    Integrate delete button for post

--- blog/views.py
+++ blog/views.py
@@ -59,3 +59,10 @@ def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    """Detete post."""
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
