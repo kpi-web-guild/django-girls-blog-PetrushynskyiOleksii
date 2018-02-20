a919b7bd0350f70fcc78c1a75e692b9f5f8f0563
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-20 10:29:24 +0200

    Add post publish view

--- blog/views.py
+++ blog/views.py
@@ -52,3 +52,10 @@ def post_draft_list(request):
     """Render page with list of drafts posts."""
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    """Publish post."""
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
