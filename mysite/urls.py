f59c4bc91f5c226b0b9bb3a70d4a0669c6cd3876
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-21 17:24:03 +0200

    Trans templates and fix URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 
 from django.contrib.auth import views
 
+from .settings import LOGIN_REDIRECT_URL
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': LOGIN_REDIRECT_URL}),
     url(r'', include('blog.urls')),
 ]
